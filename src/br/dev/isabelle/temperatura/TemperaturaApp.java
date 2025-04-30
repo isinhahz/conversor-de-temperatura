cbfe945fc90dbe68bc8837e33d6cc385bfca74f8
ISABELLE DOS SANTOS DE ABREU <25132976@dsn127m1242315.cfp127aluno.local>
2025-04-30 17:00:19 -0300

    criando classe temperatura

--- src/br/dev/isabelle/temperatura/TemperaturaApp.java
+++ src/br/dev/isabelle/temperatura/TemperaturaApp.java
@@ -3,7 +3,7 @@ package br.dev.isabelle.temperatura;
 public class TemperaturaApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
--- src/model/Temperatura.java
+++ src/model/Temperatura.java
@@ -1,5 +1,29 @@
 package model;
 
 public class Temperatura {
+	
+	private double celsius;
+
+
+public double getCelsius() {
+	return celsius;
+}
+
+public void setCelsius(double celsius) {
+	this.celsius = celsius;
+}
+	
+public double converterParaKelvin() {
+	double kelvin = (celsius + 273);
+	return celsius;
+}
+
+public double converterParaFahreinheit() {
+	return celsius;
+}
+
 
 }
+
+
+
